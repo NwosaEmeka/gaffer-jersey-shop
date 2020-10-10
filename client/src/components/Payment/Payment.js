bd8f23816332a2d388b48c791184b4855453833b
nwosaemeka <mykev.paul@gmail.com>
2020-10-10 16:15:36 +0800

    updated payment

--- client/src/components/Payment/Payment.js
+++ client/src/components/Payment/Payment.js
@@ -2,14 +2,32 @@ import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 import {CurrencyFormatter} from '../../Utils/CurrencyFormatter'
 import axios from 'axios'
+import {useHistory} from 'react-router-dom'
+import { useStoreValue } from '../../contextApi/StateProvider'
 
 
 function Payment({ total }) {
   const totalPrice = total * 100 //stripe only accept cents
   const publicKey = 'pk_test_51HaM5NGKSWUdqQNS3Dull99FPGtYe2q5SJ0iLhScOra3e3z7VVY3NjBazpU93W7iuq9dpABLR1eKXM8sXMSUkTXJ00Tb36WVIT'
 
+  const [{ basket, user }, dispatch] = useStoreValue();
+  const history = useHistory()
   const onToken = (token) => {
-    console.lob(token)
+    axios.post('/payment', {
+      amount: totalPrice,
+      token
+    })
+      .then(res => {
+        alert("Your payment was successful")
+        // create and put order in database
+
+        // reset the basket
+        dispatch({
+          type: "CLEAR_BASKET"
+        })
+        // redirect to orders page
+        history.replace('/orders')
+    })
   }
   return (
     <StripeCheckout 
